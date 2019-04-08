181fd49b8b01a98e91aa90b3cd710f68b251eb9d
Batata <wellingtonog@gmail.com>
2019-04-08 09:28:23 -0300

    Inclusão de parametricação das urls e exportação em csv

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const hadless = require("puppeteer");
+const hadless = require('puppeteer');
+const fs = require('fs');
+const jsonexport = require('jsonexport');
+const urls = require('./urls.module');
 
-let scraper = async () => {
+
+
+let scraper = async (url) => {
+
     const browser = await hadless.launch({
         headless: false,
         defaultViewport: null
     });
+
     const page = await browser.newPage();
-    await page.goto("https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Goi%C3%A1s");
+    await page.goto(url);
     await page.waitFor(1000);
 
     //Scraper
     const result = await page.evaluate(() => {
         let result = [];
-        var elementsQtd = document.querySelectorAll('h3 + table').length;
+        var elementsQtd = document.querySelectorAll('h3 + p + table').length;
         for (let index = 0; index < elementsQtd; index++) {
             const h3 = document.querySelectorAll('h3 span.mw-headline')[index].textContent;
-            const table = document.querySelectorAll('h3 + table')[index];
+            const table = document.querySelectorAll('h3 + p + table')[index];
             const lines = table.querySelectorAll('tbody tr').length;
 
             for (let t = 0; t < lines; t++) {
                 const citie = table.querySelectorAll('tbody tr')[t].querySelector('td:last-child').textContent;
                 let obj = {
+                    ufname: null,
+                    ufid: null,
                     meso: h3,
                     citie: citie.replace('\n', '')
                 }
@@ -34,6 +43,36 @@ let scraper = async () => {
     return result;
 };
 
-scraper().then((mesos) => {
-    console.log(mesos[0]);
-});
+const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+    }
+}
+
+asyncForEach(urls, async (url) => {
+    await scraper(url.url).then((mesos) => {
+
+        const fullMesos = mesos.map((item) => {
+            item.ufid = url.ufid;
+            item.ufname = url.uf;
+            return item;
+        });
+        //windows: c:\temp\estado.csv
+        const filename = '/temp/' + url.uf + '.csv';
+        let csvData = null;
+
+        jsonexport(fullMesos, {
+            rowDelimiter: ';'
+        }, function (err, csv) {
+            if (err) return console.log(err);
+            csvData = csv;
+        });
+
+        fs.writeFile(filename, csvData, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Arquivo ' + url.uf + '.csv salvo!');
+        })
+    })
+})
---
+++ urls.module.js
@@ -0,0 +1,130 @@
+module.exports = [{
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Acre',
+        ufid: 12,
+        uf: 'AC'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Alagoas',
+        ufid: 27,
+        uf: 'AL'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Goi%C3%A1s',
+        ufid: 52,
+        uf: 'GO'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Mato_Grosso_do_Sul',
+        ufid: 50,
+        uf: 'MS'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Mato_Grosso',
+        ufid: 51,
+        uf: 'MT'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_da_Bahia',
+        ufid: 29,
+        uf: 'BA'
+    }, {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Cear%C3%A1',
+        ufid: 23,
+        uf: 'CE'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Maranh%C3%A3o',
+        ufid: 21,
+        uf: 'MA'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_da_Para%C3%ADba',
+        ufid: 25,
+        uf: 'PB'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Pernambuco',
+        ufid: 26,
+        uf: 'PE'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Piau%C3%AD',
+        ufid: 22,
+        uf: 'PI'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Rio_Grande_do_Norte',
+        ufid: 24,
+        uf: 'RN'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Sergipe',
+        ufid: 28,
+        uf: 'SE'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Amazonas',
+        ufid: 13,
+        uf: 'AM'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Amap%C3%A1',
+        ufid: 16,
+        uf: 'AP'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Rond%C3%B4nia',
+        ufid: 11,
+        uf: 'RO'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Roraima',
+        ufid: 14,
+        uf: 'RR'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Tocantins',
+        ufid: 17,
+        uf: 'TO'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Esp%C3%ADrito_Santo',
+        ufid: 32,
+        uf: 'ES'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Minas_Gerais',
+        ufid: 31,
+        uf: 'MG'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Rio_de_Janeiro',
+        ufid: 33,
+        uf: 'RJ'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_S%C3%A3o_Paulo',
+        ufid: 35,
+        uf: 'SP'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Paran%C3%A1',
+        ufid: 41,
+        uf: 'PR'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Rio_Grande_do_Sul',
+        ufid: 43,
+        uf: 'RS'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_de_Santa_Catarina',
+        ufid: 42,
+        uf: 'SC'
+    },
+    {
+        url: 'https://pt.wikipedia.org/wiki/Lista_de_mesorregi%C3%B5es_e_microrregi%C3%B5es_do_Par%C3%A1',
+        ufid: 15,
+        uf: 'PA'
+    }
+]
